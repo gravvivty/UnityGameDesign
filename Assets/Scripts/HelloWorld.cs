a28f05c48a5641b0e5e5e904f8faf14a7908f979
Duy Nguyen <nahnduy@gmail.com>
2025-04-14 15:56:17 +0200

    Test Commit Duy

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -5,12 +5,12 @@ public class HelloWorld : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Duy: Hello, World!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
