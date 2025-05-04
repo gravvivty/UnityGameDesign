68b8db4c49f23d4eac2899324c64e47a6f251523
gravvivty <gayer.steven@gmail.com>
2025-05-04 05:14:53 +0200

    Added Inventory and Inventory UI on "I" - fix icon not being shown

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Project.Inventory
+{
+    /// <summary>
+    /// Singleton class that manages the player's inventory by storing and checking collected items.
+    /// </summary>
+    public class InventoryManager : MonoBehaviour
+    {
+        public static InventoryManager Instance { get; private set; }
+
+        private List<ItemData> items = new List<ItemData>();
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        /// <summary>
+        /// Adds an item to the inventory and logs its details.
+        /// </summary>
+        /// <param name="itemData">The item to be added.</param>
+        public void AddItem(ItemData itemData)
+        {
+            items.Add(itemData);
+            Debug.Log($"Picked up item: {itemData.itemName} (ID: {itemData.itemID})");
+        }
+
+        /// <summary>
+        /// Checks whether an item with a specific ID exists in the inventory.
+        /// </summary>
+        /// <param name="itemID">The ID of the item to check for.</param>
+        /// <returns>True if the item is found, false otherwise.</returns>
+        public bool HasItemWithID(int itemID)
+        {
+            foreach (ItemData item in items)
+            {
+                if (item.itemID == itemID)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        /// <summary>
+        /// Checks whether an item with a specific name exists in the inventory.
+        /// </summary>
+        /// <param name="itemName">The name of the item to check for.</param>
+        /// <returns>True if the item is found, false otherwise.</returns>
+        public bool HasItemWithName(string itemName)
+        {
+            foreach (ItemData item in items)
+            {
+                if (item.itemName == itemName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        
+        /// <summary>
+        /// Returns a copy of the current inventory list.
+        /// </summary>
+        /// <returns>A new list containing all items in the inventory.</returns>
+        public List<ItemData> GetAllItems()
+        {
+            return new List<ItemData>(items);
+        }
+    }
+}
