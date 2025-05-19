731871382245d26102babc86326f07fd593ad17f
gravvivty <gayer.steven@gmail.com>
2025-05-19 19:33:48 +0200

    Added Combine in Scene

---
+++ Assets/Scripts/Interactables/InSceneInteract/WellReceiver.cs
@@ -0,0 +1,37 @@
+﻿using Project.Inventory;
+using UnityEngine;
+
+namespace Project.Interactable.InSceneInteract
+{
+    public class WellReceiver : ItemReceiver
+    {
+        public override bool TryUseItem(ItemData draggedItem)
+        {
+            // Check for a valid combination
+            if (draggedItem.CanCombine(itemRepresentation.itemID))
+            {
+                ItemData result = draggedItem.GetCombinationResult(itemRepresentation.itemID);
+                Debug.Log($"Combined {draggedItem.itemName} with {itemRepresentation.itemName} to get {result.itemName}");
+                
+                // CUSTOM LOGIC -----
+                if (spriteRenderer != null && itemRepresentation.itemName == "Well")
+                {
+                    itemRepresentation = result;
+                    spriteRenderer.sprite = itemRepresentation.icon;
+                }
+                if (spriteRenderer != null && itemRepresentation.itemName == "WellRope")
+                {
+                    itemRepresentation = result;
+                    spriteRenderer.sprite = itemRepresentation.icon;
+                    // Do something else idk, add something to  the inventory or sum shit
+                }
+
+                return true;
+                // CUSTOM LOGIC ----
+            }
+
+            Debug.Log("Can't use this item on the Well.");
+            return false;
+        }
+    }
+}
