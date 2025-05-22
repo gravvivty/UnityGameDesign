274df564cb0096a12d38bd17e4476fc4a9738a16
gravvivty <gayer.steven@gmail.com>
2025-05-23 00:35:33 +0200

    Fixed Omi not panicking after scene load

--- Assets/Scripts/Scene/Level1/SceneControllerLvl1.cs
+++ Assets/Scripts/Scene/Level1/SceneControllerLvl1.cs
@@ -16,37 +16,41 @@ namespace Project.Scene.SceneControllerLvl1
 
         void Start()
         {
-            StartCoroutine(wait());
+            StartCoroutine(InitializeScene());
+        }
+
+        private IEnumerator InitializeScene()
+        {
+            yield return new WaitForEndOfFrame(); // ensures objects are fully initialized
+
             if (PlayerPrefs.GetInt("isLit", 0) == 1)
             {
-                if (villageDoor != null)
-                {
-                    villageDoor.SetActive(true);
-                }
+                if (villageDoor != null) villageDoor.SetActive(true);
+
                 fireGO.SetActive(true);
-                Animator fireAnimator = fireGO.GetComponent<Animator>();
-                fireAnimator.Play("FireIdle",0,0f);
+                var fireAnimator = fireGO.GetComponent<Animator>();
+                fireAnimator.Play("FireIdle", 0, 0f);
+
                 if (omi.activeSelf)
                 {
                     Animator omiAnimator = omi.GetComponent<Animator>();
                     Debug.Log("PanicOmi? " + omiAnimator.HasState(0, Animator.StringToHash("PanicOmi")));
                     omiAnimator.Play("PanicOmi", 0, 0f);
-                    Debug.Log("Current animation state: " + omiAnimator.GetCurrentAnimatorStateInfo(0).IsName("PanicOmi"));
                 }
 
                 if (omi_noCurtain.activeSelf)
                 {
                     Animator omiNoCurtainAnimator = omi_noCurtain.GetComponent<Animator>();
-                    omiNoCurtainAnimator.Play("PanicOmi",0,0f);
+                    omiNoCurtainAnimator.Play("PanicOmi", 0, 0f);
                 }
+
                 guard.GetComponent<NPCMovement>().enabled = false;
                 Animator guardAnimator = guard.GetComponent<Animator>();
-                guardAnimator.Play("FireGuard",0,0f);
+                guardAnimator.Play("FireGuard", 0, 0f);
                 guard.transform.localPosition = new Vector3(-0.6f, 1.6f, 0f);
                 guard.GetComponent<SpriteRenderer>().sortingLayerName = "NPC";
             }
         }
-
         void Update()
         {
             if (InventoryManager.Instance.HasItemWithID(56))
